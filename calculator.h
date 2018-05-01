68a977833ad9f5502e49ab60a036bc1bea2c8cd2
William-Tweedale <38878847+William-Tweedale@users.noreply.github.com>
2018-05-01 18:51:54 +1000

    Update calculator.h
    function declarations for add and multiply

--- calculator.h
+++ calculator.h
@@ -1,3 +1,5 @@
 double divide (double N1, double N2);
 double subtract (double N1, double N2);
+double add(double *numbers_p, int count);
+double multiply(double *numbers_p, int count);
 void debug (string ARG);
