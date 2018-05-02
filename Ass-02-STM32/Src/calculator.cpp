39f584545e16abe5760c1220c927cd323b763559
pingu2211 <vk2fbha@gmail.com>
2018-05-02 19:30:10 +1000

    Prepear calculator.cpp for change in function inputs

--- Ass-02-STM32/Src/calculator.cpp
+++ Ass-02-STM32/Src/calculator.cpp
@@ -3,34 +3,32 @@
 #include <stdio.h>
 #include<stdint.h>
 
-double divide (double N1, double N2) {
-	return N1 / N2;
+int8_t divide (uint8_t *numbers_p[], uint8_t count) {
 }
 
-double subtract (double N1, double N2) {
-	return N1 - N2;
+int8_t subtract (uint8_t *numbers_p[], uint8_t count) {
 }
 
 int8_t add(uint8_t *numbers_p[], uint8_t count) {
 
-	double sum = 0;
+/*	double sum = 0;
 
 	for (int i = 0; i < count; i++)
 	{
 		sum += numbers_p[i];
 	}
-	return sum;
+	return sum;*/
 }
 
 
 
 int8_t multiply(uint8_t *numbers_p[], uint8_t count) {
 
-	double sum = 1;
+/*	double sum = 1;
 
 	for (int i = 0; i < count; i++)
 	{
 		sum *= numbers_p[i];
 	}
-	return sum;
+	return sum;*/
 }
