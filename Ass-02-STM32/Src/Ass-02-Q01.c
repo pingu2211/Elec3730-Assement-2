f80191d27987a114639a92fde4afb0f1564798d7
pingu2211 <vk2fbha@gmail.com>
2018-05-02 17:19:12 +1000

    Started implimenting button structers

--- Ass-02-STM32/Src/Ass-02-Q01.c
+++ Ass-02-STM32/Src/Ass-02-Q01.c
@@ -10,10 +10,10 @@
 enum CONTROL_CHARS {NUL=0,SOH,STX,ETX,EOT,ENQ,ACK,BEL,BS,TAB,LF,VT,FF,CR,SO,SI,DLE,DC1,DC2,DC3,DC4,NAK,SYN,ETB,CAN,EM,SUB,ESC,FS,GS,RS,US=31,DEL=127};
 
 int isControlChar(char c){
-	int code;
 	if (c<32||c==127) return true;
 	else return false;
 }
+
 
 
 void CommandLineParserInit(void)
