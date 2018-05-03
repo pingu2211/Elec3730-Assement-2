d4bc09bb9b1f87d115f4693c604c65f9dda135a6
William-Tweedale <38878847+William-Tweedale@users.noreply.github.com>
2018-05-03 11:58:37 +1000

    Added Ass-1 Q-4 plus some Q1 comments

--- Ass-02-STM32/Src/Ass-02-Q01.c
+++ Ass-02-STM32/Src/Ass-02-Q01.c
@@ -34,28 +34,97 @@ void CommandLineParserProcess(void)
 #ifdef STM32F407xx
   if (HAL_UART_Receive(&huart2, &c, 1, 0x0) == HAL_OK) //Receive a new character and store in c
   {
-	HAL_GPIO_TogglePin(GPIOD, LD4_Pin); // Toggle LED4
-	if (!isControlChar(c)){
+	HAL_GPIO_TogglePin(GPIOD, LD4_Pin); 				// Toggle LED4
+	if (!isControlChar(c)){			// check that user entered a character and not a control character
 		printf("%c", c);
-		// store in the next element of array and increes counter by 1
+		array_of_inputs[count]=c;	// store in the next element of array
+		count++;					// increase counter by 1
 
-		array_of_inputs[count]=c;
-		count++;
 	} else {
 		switch (c){
-		case CR :
+		case CR :			// case 1: if user presses "enter"
 			printf("\n");
 			// add \0 to the end of the string and then give it to q3 of assesment 1
-			array_of_inputs[count] = NULL;
+			array_of_inputs[count] = NULL;	// add \0 to the end of the string
 			break;
-		case DEL:
+		case DEL:			// case 2: if user presses "backspace"
 			printf("\b");
-			// subtract 1 from count
+			count--;		// subtract 1 from count
+
 			break;
 
-		default:
-			printf(" %i ",c);
+		default:				// when a control character is entered that is NOT enter or backspace
+			printf(" %i ",c);	// print the askii number of the control character that was entered
 		}
+	}
+	int string_parser(char *array_of_inputs, char **array_of_strings_p[])
+	{
+		/* declare all the things */
+			int inp_count=0;
+			int j = 0;
+			char **array_of_strings;	// this particular one helps me wrap my head arround pointers to pointers to pointers.....
+
+			int input_length = strlen (array_of_inputs);			// make sure the input string actually has something in it
+			if (input_length <= 0) return -1;						// if input is empty return error
+
+			array_of_strings = (char**)malloc(sizeof( *array_of_strings) * input_length);	// allocate some memory, for now we'll assume ever character is its own word and trim it down later
+			if (!array_of_strings) {														//make sure  memory was allocated successfully
+				printf ("\nError in String Passer\nError reallocating memory");
+				return -1;
+			}
+			array_of_strings[inp_count] = (char*)malloc(input_length);	// allocate some memory to the first word, and assume its going to be as long as the entire input string
+			if (!array_of_strings[inp_count]) { 						//make sure memory was allocated successfully
+				printf ("\nError in String Passer\nError reallocating memory");
+				return -1;
+			}
+
+			for (int i = 0; array_of_inputs[i] != NULL; i++) {				// start at the start of the input string, stop at the null terminator, move along by 1 each time
+				if (array_of_inputs[i]!=' ') {								// if this character is NOT a space, put it in the current word
+					array_of_strings[inp_count][j] = array_of_inputs[i];
+					j++;										// j is for keeping track of how long the current word is
+				}
+				else if (j>0) {									// if the current character in the input is a space, AND the last character wasnt a space either
+					array_of_strings[inp_count][j] = '\0';											// add a null terminator
+					array_of_strings[inp_count] = (char*)realloc(array_of_strings[inp_count],j+1);	// trim the array down, save some memmory, we didnt always have thousands of bits to play with laddy
+					if (!array_of_strings[inp_count]) {												// make sure that went ok.... or fail gracefully
+						printf ("\nError in String Passer\nError reallocating memory");
+						return -1;
+					}
+					inp_count++;													// HEY!! we just got to the end of the word!! should keep an eye on how many we've got so far
+					array_of_strings[inp_count] = (char*)malloc (input_length-i);	//allocate some memory,and we wont ever need more than the lengh of the input array less what we've already dealt with
+					if (!array_of_strings[inp_count]) { //make sure that went ok or fail gracefully
+						printf ("\nError in String Passer\nError reallocating memory");
+						return -1;
+					}
+					j = 0;																			// and make sure the other bit back up there ^^ knows we started a new word
+				}
+				else {
+					continue;	// if we got here there was more than one space in a row sooooo... ignore that and carry on
+				}
+			}
+			// HEY were at the end of the input string!!
+			array_of_strings[inp_count][j] = '\0';	// dont forget the null terminator on the last word!!
+			array_of_strings[inp_count] = (char*)realloc (array_of_strings[inp_count], j + 1);	//or to trim down the last word, remember what i said about saving bits,
+			if (!array_of_strings[inp_count]) {	// if that didnt go well, probbly tell some one about it
+				printf ("\nError in String Passer\nError reallocating memory\n");
+				return -1;
+			}
+			if (strlen(array_of_strings[inp_count])>0){
+				inp_count++; //make sure not to forget that last word!! Just be careful the last character wasn't a space
+			}
+
+
+
+			array_of_strings = (char**)realloc (array_of_strings, sizeof (*array_of_strings) * inp_count);// OH and remember how we thought every character might be its own word?? well it wasnt, so lets shrink this chunk of memmory down a bit
+			if (!array_of_strings[inp_count]) {	// if that didnt go well... why not we were making the damn thing smaller????????? still tell some one though
+				printf ("\nError in String Passer\nError reallocating memory\n");
+				return -1;
+			}
+
+
+			*array_of_strings_p = array_of_strings; // let the boss upstrairs know where we put all of those words so he can find'm later
+			return inp_count;					// and we should probably report how many we did so he doesnt go looking for too many
+
 	}
 
 //checking the input
